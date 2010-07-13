a9129ec0322074fdd03b3648c7d8a3fed58cdc51
Marcin Bunsch <marcin@applicake.com>
2010-07-13 14:13:14 +0200

    Refactored spec engine

---
+++ shared/spec.rb
@@ -0,0 +1,6 @@
+CHALLENGE_BASE_PATH = File.expand_path(File.dirname(__FILE__) + '/../')
+
+require File.dirname(__FILE__) + '/challenge/dsl_accessor'
+require File.dirname(__FILE__) + '/challenge/solution'
+require File.dirname(__FILE__) + '/challenge/spec'
+
