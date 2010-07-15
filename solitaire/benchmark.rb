c91bb8f5b886a0736a34a5bcda809729737506fc
Marcin Bunsch <marcin@applicake.com>
2010-07-15 10:21:07 +0200

    Corrected benchmark

--- solitaire/benchmark.rb
+++ solitaire/benchmark.rb
@@ -7,14 +7,8 @@ Challenge::Benchmark.new do
     @instance = klass.new
   end
   action do
-    secret = @instance.encrypt('Aa@' * 5000)
+    secret = @instance.encrypt('Aa@' * 50000)
     plain = @instance.decrypt secret
   end
 end
 
-# 'Aa@' * 50000
-# solitaire - bundu: 88.7629518508911 seconds
-# 'Aa@' * 5000
-# solitaire - bundu: 8.55243992805481 seconds
-# 'Aa@' * 500 
-# solitaire - bundu: 0.8348069190979 seconds
