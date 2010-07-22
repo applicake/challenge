6441fd1dd1d8163789fc0c0911f2a8534037c38e
mariusz <mariusz@applicake.com>
2010-07-22 14:47:53 +0200

    New solution from Jacek. Unfortunately doesn't work, either.

--- gem_dependency_resolver/solutions/jacek.rb
+++ gem_dependency_resolver/solutions/jacek.rb
@@ -1,3 +1,4 @@
+# FINAL 1
 class Jacek
   def resolve(gems)
     JaceksResolver.new(gems).resolve
@@ -5,31 +6,73 @@ class Jacek
 end
 
 class JaceksResolver
-  attr_reader :gems, :gem_hash, :gem_hash_mapped, :gem_tables, :result, :sorted_gem_tables, :sorted_gem_tables_with_includes
+  attr_reader :gems, :gem_hash, :gem_tables, :result, :sorted_gem_tables, :sorted_gem_tables_with_includes
 
   def initialize(gem_hash)
-    @gem_hash = gem_hash
-    @gems = gem_hash.keys
-    @gem_count = @gems.nitems
-    @gem_hash_mapped= @gem_hash.inject({}) {|sum, k| sum.merge({@gems.index(k[0]) => k[1].collect{|x| @gems.index(x)}})}
-    @gem_tables = @gem_hash_mapped.inject([]) {|sum, k| sum << k.flatten.uniq}
+#    PerfTools::CpuProfiler.start("/home/jacek/dupa_profile") do
+  #  t1 = Benchmark.measure do
+      @gem_hash = gem_hash
+      @gems = gem_hash.keys
+      @gem_count = @gems.nitems
+   # end
+   # t2 = Benchmark.measure do
+      map_hash
+  #  end
+ #   t3 = Benchmark.measure do
+      add_includes_to_tables
+ #   end
+#    t4 = Benchmark.measure do
+      resolve
+#    end
+#    puts t1,t2,t3,t4
+#    end
+  end
+
+  def map_hash
+    @gem_tables = @gem_hash.inject([]) {|sum, k| sum << k.flatten.uniq}
+  end
+
+  def add_includes_to_tables
     @gem_tables_with_includes = @gem_tables.collect do |arr|
+      required = arr
       begin
-        required = @gem_hash_mapped.values_at(*arr).flatten.uniq.compact - arr
+        required = @gem_hash.values_at(*required).flatten.uniq.compact - required
+        unless (required & arr).empty?
+          raise self.exception
+        end
         arr += required
       end until(required.empty?) 
       arr
     end
-    resolve
+#    @gem_tables_with_includes = @gem_tables
   end
 
   def resolve
     @sorted_gem_tables_with_includes = @gem_tables_with_includes.sort {|x,y| x_i_y = x.include?(y.first); y_i_x = y.include?(x.first); x_i_y ? (y_i_x ? (raise self.exception) : 1) : (y_i_x ? -1 : 0)}
-    @result = @sorted_gem_tables_with_includes.collect {|x| @gems[x.first]}
+    @result = @sorted_gem_tables_with_includes.map &:first
   end
 
   def exception
     Exception.new 'Loop found!'
   end
 
+  def verbose
+    puts self.gems.inspect
+    puts self.gem_hash.inspect
+    puts self.gem_hash_mapped.inspect
+    puts self.gem_tables.inspect
+    puts self.result.inspect
+
+    puts "--------------------"
+    puts self.sorted_gem_tables_with_includes.collect {|t| t.inspect}
+    puts "--------------------"
+  end
+
+  def to_s
+    self.gems.inspect
+  end
+
+  def inspect
+    self.to_s
+  end
 end
