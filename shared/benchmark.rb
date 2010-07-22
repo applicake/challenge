3c719c56b392db97451e4207add29dce8b65cac6
Marcin Bunsch <marcin@applicake.com>
2010-07-22 11:18:28 +0200

    Improved Benchmark, added support for iterations and prettied up labels

--- shared/benchmark.rb
+++ shared/benchmark.rb
@@ -2,6 +2,7 @@ require 'benchmark'
 
 CHALLENGE_BASE_PATH = File.expand_path(File.dirname(__FILE__) + '/../')
 
+
 require File.dirname(__FILE__) + '/challenge/dsl_accessor'
 require File.dirname(__FILE__) + '/challenge/benchmark'
 require File.dirname(__FILE__) + '/challenge/solution'
--- shared/challenge/benchmark.rb
+++ shared/challenge/benchmark.rb
@@ -1,6 +1,7 @@
 require 'stringio'
 module Challenge
   class Benchmark
+    include ::Benchmark
     extend DslAccessor
     dsl_accessor :name, :iterations, :action, :before, :use, :solutions, :solution, :verbose
 
@@ -9,30 +10,42 @@ module Challenge
       instance_eval(&block)
       use ARGV[0] 
       if !use
+        solutions = Challenge::Solution.load(self.name)
+        puts "Challenge: #{self.name}\n"
+        puts "Running #{solutions.size} solution(s) at #{iterations} iteration(s)...\n\n"
+        
         # If nothing is supplied, run all, but in separate processes
-        Challenge::Solution.load(self.name).each { |solution| system "ruby benchmark.rb #{solution.name}" }
+        solutions.each { |solution| system "ruby benchmark.rb #{solution.name} --quiet" }
+        puts "\nThe given time is an average for one iteration"
         exit
+      end
+      unless ARGV.include?('--quiet')
+        puts "Challenge: #{self.name}\n\n"
       end
       solution Challenge::Solution.find(self.name, use)
       perform
+      puts "\nThe given time is an average for one iteration" unless ARGV.include?('--quiet')
     end
     
     def perform
       before.call if before
       time = nil
       $stdout, old_stdout = StringIO.new, $stdout unless verbose
-      ::Benchmark.bm(iterations) do |x|
-        report = x.report("#{name} - #{use}:") do
-          action.call
-        end
-        time = report.real
+      report = measure do
+        iterations.times { action.call }
       end
+      time = report.real / iterations.to_f # get the average time
       $stdout = old_stdout unless verbose
       render time
     end
     
     def render(time)
-      puts "#{name} - #{use}: #{time} seconds"
+      width = 12
+      if use.length < width
+        puts "#{use}:#{' ' * (width - use.length + 1)} #{time} seconds"
+      else
+        puts "#{use[0..width]}: #{time} seconds"
+      end
     end
     
   end
