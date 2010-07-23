b71aa51169806cb4f325dac6d2ce178091a16072
Michał Taszycki <mtaszycki@gmail.com>
2010-07-24 00:50:12 +0200

    specification added

--- hashish/solutions/hashish.rb
+++ hashish/solutions/hashish.rb
@@ -1,3 +1,16 @@
 class Hashish
-
+  def store(key, value)
+  end
+  
+  def fetch(key)
+  end           
+   
+  def delete(key)
+  end       
+   
+  def has_key?(key)
+  end         
+  
+  def length
+  end
 end
