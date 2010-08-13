52cc270e4f0d4808020e7338fac616ad3eeb0c90
Agata Mazur <agata@agata.com>
2010-08-13 17:38:34 +0200

    longest repeated substring

---
+++ substring/spec/longest_repeated_substring_spec.rb
@@ -0,0 +1,31 @@
+require File.dirname(__FILE__) + '/../../shared/spec' unless defined?(Challenge)
+require 'ruby-debug'
+
+Challenge::Spec.new do
+
+  before do
+    klass = @solution.constant
+    @substring = klass.new
+  end
+  
+  spec "Banan test" do
+    @substring.fetch_substring("banan")).should == "an" 
+  end
+  
+  spec "No substring text" do
+    @substring.fetch_substring('norepeatedsubstring').should == ''
+  end
+  
+  spec "Text with spaces" do
+    @substring.fetch_substring('repeateated substring with spaces').should == 'eat'
+  end
+  
+  spec "Text with two substrings of same length" do
+    @substring.fetch_substring('doubledoublestringstring').should == 'double string'
+  end
+  
+  spec "Very long word test" do
+      @substring.fetch_substring("verylongwordlonglongblebleblelongwordlongwordlongveryveryverybleverylongwordveryveryveryverylongwordveryveryverybleblewordword").should == 'verylongwordveryveryvery'
+  end
+    
+end
