29e33df41ebe0157434fedb02f5810fb30998303
cfx <cfx@cfx-desktop.(none)>
2010-09-08 17:18:18 +0200

    benchmark fixed

--- ip2country/benchmark.rb
+++ ip2country/benchmark.rb
@@ -5,13 +5,16 @@ Challenge::Benchmark.new do
   before do
     @klass = solution.constant
   end
+
   action do
-    @instance = @klass.new
-    @instance.find_country_by_ip('83.29.122.34')
-    @instance.find_country_by_ip('68.97.89.187')
-    @instance.find_country_by_ip('80.79.64.128')
-    @instance.find_country_by_ip('192.189.119.1')
-    @instance.find_country_by_ip('210.185.128.123')
-    @instance.find_country_by_ip('255.0.0.1')
+    ['83.29.122.34',
+     '68.97.89.187', 
+     '80.79.64.128', 
+     '192.189.119.1', 
+     '210.185.128.123', 
+     '255.0.0.1'].each do |ip|
+
+      @klass.new.find_country_by_ip(ip)
+     end
   end
 end                             
