26a72db6ac11912ed673f1f1aac1882e05d72eda
Dominik Fijas <dominik@applicake.com>
2010-11-15 10:59:31 +0100

    modified spec file

--- change/spec/change_spec.rb
+++ change/spec/change_spec.rb
@@ -13,9 +13,10 @@ Challenge::Spec.new do
     @money.change(15, [4, 2, 1]).should =~ [4, 4, 4, 2, 1]
     @money.change(25, [10, 5]).should =~ [10, 10, 5]
     @money.change(9, [3, 2, 1]).should =~ [3, 3, 3]
-    @money.change(9, [2]).should =~ [2, 2, 2, 2]
+    @money.change(9, [2]).should == []
     @money.change(10, []).should == []
     @money.change(10, [20]).should == []
+    @money.change(5, [3,4]).should == []
   end
   
 end
